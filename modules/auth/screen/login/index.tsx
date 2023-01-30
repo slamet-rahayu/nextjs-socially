aacd43f01675ac6a13758740304e8a9bac599cf9
slamet-rahayu <slamet_rahayu93@yahoo.com>
2023-01-30 21:09:31 +0700

    add header

--- modules/auth/screen/login/index.tsx
+++ modules/auth/screen/login/index.tsx
@@ -1,11 +1,23 @@
-import ContainerModule from 'modules/container/screen/layout';
 import React from 'react';
+import { ChevronLeft } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import Header from 'components/header';
+import ContainerModule from 'modules/container/screen/layout';
+import { useRouter } from 'next/router';
 
 export default function LoginScreen(): React.ReactElement {
+  const router = useRouter();
   return (
     <ContainerModule withBottomTab={false}>
+      <Header
+        leftComponent={
+          <IconButton onClick={() => router.replace('/')}>
+            <ChevronLeft />
+          </IconButton>
+        }
+      />
       <div className="px-6 py-4">
-        <div className="mt-20">
+        <div className="mt-12">
           <p className="text-lg font-semibold">Sign In</p>
           <p>Enter your credentials</p>
         </div>
@@ -15,30 +27,32 @@ export default function LoginScreen(): React.ReactElement {
             <input
               type="text"
               placeholder="susu@moo.com"
-              className="bg-gray-50 w-full p-3 rounded-xl outline-none"
+              className="bg-gray-50 w-full p-3 rounded-xl outline-[#006175] border-2"
             />
           </div>
           <div className="mb-3">
             <p className="mb-2 font-semibold text-sm">Password</p>
             <input
-              type="text"
-              placeholder="susu@moo.com"
-              className="bg-gray-50 w-full p-3 rounded-xl outline-none"
+              type="password"
+              placeholder="Something secret"
+              className="bg-gray-50 w-full p-3 rounded-xl outline-[#006175] border-2"
             />
           </div>
           <button type="button" className="text-[#1C6758] text-xs float-right">
             Forgot Password?
           </button>
         </div>
-        <div className="fixed max-w-md w-full px-6 left-0 bottom-12">
-          <button type="button" className="w-full py-3 rounded-xl bg-[#1C6758]">
-            <p className="font-semibold text-center text-white">Login</p>
-          </button>
-          <div className="flex font-semibold text-sm mt-3 justify-center">
-            <p className="mr-1">Do not have an Account?</p>
-            <button type="button" className="text-[#1C6758]">
-              Sign Up
+        <div className="fixed w-full flex justify-center px-6 left-0 bottom-12">
+          <div className="max-w-md w-full px-4">
+            <button type="button" className="w-full py-3 rounded-xl bg-[#1C6758]">
+              <p className="font-semibold text-center text-white">Login</p>
             </button>
+            <div className="flex font-semibold text-sm mt-3 justify-center">
+              <p className="mr-1">Do not have an Account?</p>
+              <button type="button" className="text-[#1C6758]">
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
       </div>
